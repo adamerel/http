0b5cc730f084891b7e13dbaea23afe7290713b32
adamer <152396897+adamerel@users.noreply.github.com>
2025-01-16 14:02:07 +0100

    Create contador-cookie.php

---
+++ contador-cookie.php
@@ -0,0 +1,29 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$servername = "localhost";
+$username = "admin";
+$password = "12345";
+$dbname = "estadistics";
+
+// Crear conexión
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verificar conexión
+if ($conn->connect_errno) {
+    echo "Error en connectar a MySQL: " . $conn->connect_error;
+    exit();
+}
+
+$ip = $_SERVER['REMOTE_ADDR'];
+$sql = "INSERT INTO registre(ip) VALUES ('$ip')";
+$conn->query($sql);
+
+$resultat = $conn->query("SELECT COUNT(*) FROM registre");
+$row = mysqli_fetch_array($resultat);
+echo $row[0];
+
+$conn->close();
+?>
